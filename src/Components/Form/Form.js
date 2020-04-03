8d6e80611dbf42a8e7aa2f551396992ff1c1c315
Raxa Hansana <raxah@Raxas-MacBook-Pro.local>
2020-04-03 11:22:04 -0700

    completed stage 1 - step 1 / form completed w/out submit button

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,10 +1,75 @@
 import React, { Component } from 'react';
 
 export default class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      price: 0,
+      imgurl: '',
+    };
+    this.handleName = this.handleName.bind(this);
+    this.handlePrice = this.handlePrice.bind(this);
+    this.handleImgUrl = this.handleImgUrl.bind(this);
+    // to clear state values
+    this.baseState = this.state;
+    this.resetState = this.resetState.bind(this);
+  }
+
+  handleName(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  handlePrice(e) {
+    this.setState({ price: e.target.value });
+  }
+
+  handleImgUrl(e) {
+    this.setState({
+      imgurl: e.target.value,
+    });
+  }
+
+  resetState() {
+    this.setState(this.baseState);
+  }
+
   render() {
+    // console.log(this.state);
     return (
       <div>
         <h1>Form Component</h1>
+        <form>
+          <label for='imgurl'>Image Url:</label>
+          <input
+            type='text'
+            id='imgurl'
+            name='imgurl'
+            value={this.state.imgurl}
+            onChange={this.handleImgUrl}
+          />
+
+          <label for='productname'>Product Name:</label>
+          <input
+            type='text'
+            id='productname'
+            name='productname'
+            value={this.state.name}
+            onChange={this.handleName}
+          />
+
+          <label for='price'>Price:</label>
+          <input
+            type='text'
+            id='price'
+            name='price'
+            value={this.state.price}
+            onChange={this.handlePrice}
+          />
+
+          <input type='button' value='Cancel' onClick={this.resetState} />
+          <input type='submit' value='Add to Inventory' />
+        </form>
       </div>
     );
   }
